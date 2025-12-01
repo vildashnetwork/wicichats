c39a999991b032814966f8576226b5836f85e63e
Li Blissz <liblissz3@gmail.com>
2025-12-01 13:59:43 +0100

    first commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -12,6 +12,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "https://wicikichatbackend.onrender.com/api" : "/api",
+
 });
 
 axiosInstance.interceptors.request.use((config) => {
