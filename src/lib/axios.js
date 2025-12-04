59034f1040254e392e75747c01a7aa82f5b49d41
Li Blissz <liblissz3@gmail.com>
2025-12-04 14:15:01 +0100

    the axios configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -7,30 +7,12 @@
 
 // export default axiosInstance;
 
-// src/lib/axios.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//     baseURL: import.meta.env.MODE === "development" ? "https://wicikichatbackend.onrender.com/api" : "/api",
-
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) config.headers.Authorization = `Bearer ${token}`;
-//     return config;
-// });
-
-// export default axiosInstance;
+// src / lib / axios.js
 import axios from "axios";
 
-const BASE_URL =
-    import.meta.env.MODE === "development"
-        ? "http://localhost:4000/api" // your local backend
-        : "https://wicikichatbackend.onrender.com/api"; // your deployed backend
-
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: import.meta.env.MODE === "development" ? "https://wicikichatbackend.onrender.com/api" : "/api",
+
 });
 
 axiosInstance.interceptors.request.use((config) => {
@@ -40,3 +22,24 @@ axiosInstance.interceptors.request.use((config) => {
 });
 
 export default axiosInstance;
+
+
+
+// import axios from "axios";
+
+// const BASE_URL =
+//     import.meta.env.MODE === "development"
+//         ? "http://localhost:4000/api" // your local backend
+//         : "https://wicikichatbackend.onrender.com/api"; // your deployed backend
+
+// const axiosInstance = axios.create({
+//     baseURL: BASE_URL,
+// });
+
+// axiosInstance.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
+//     return config;
+// });
+
+// export default axiosInstance;
